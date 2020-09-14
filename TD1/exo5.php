6b731e878fff9e0c8ae10cdeea9e8e952f6803c6
naberte <enzo.nabert@etu.umontpellier.fr>
2020-09-14 10:58:33 +0200

    exo 5.3

--- TD1/exo5.php
+++ TD1/exo5.php
@@ -5,11 +5,17 @@
 </head>
 <body>
 	<?php 
-	$immatriculation="256AB34";
-	$marque="Renault";
-	$couleur="bleue";
-	echo "Voiture $immatriculation de marque $marque (couleur $couleur)";
+	// $immatriculation="256AB34";
+	// $marque="Renault";
+	// $couleur="bleu";
+	// echo "Voiture $immatriculation de marque $marque (couleur $couleur)";
+	
+	$voiture = array(
+		'immatriculation' => '256AB34' ,
+		'marque' => 'Renault',
+		'couleur' => 'bleu'
+	);
+	echo "Voiture {$voiture['immatriculation']} de marque {$voiture['marque']} (couleur {$voiture['couleur']})";
 	?>
 </body>
-</html;
->
+</html>
