2849919ffe9dedc110ae85e107c7c62d087ca733
naberte <enzo.nabert@etu.umontpellier.fr>
2020-09-14 10:50:28 +0200

    exo 5.2

---
+++ TD1/exo5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>exercice 5</title>
+</head>
+<body>
+	<?php 
+	$immatriculation="256AB34";
+	$marque="Renault";
+	$couleur="bleue";
+	echo "Voiture $immatriculation de marque $marque (couleur $couleur)";
+	?>
+</body>
+</html;
+>
