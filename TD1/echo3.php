c60e42385d5d87ec2fa03eb34b70728571323966
naberte <enzo.nabert@etu.umontpellier.fr>
2020-09-14 10:29:12 +0200

    test différents echo

---
+++ TD1/echo3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php 3</title>
+</head>
+<body>
+	<?php 
+	$prenom = "Marc";
+
+	echo "Bonjour " . $prenom;
+	echo "Bonjour $prenom";
+	echo 'Bonjour $prenom';
+
+	echo $prenom;
+	echo "$prenom";
+
+ ?>
+</body>
+</html>
