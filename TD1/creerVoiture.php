dffee682ec196e4f89ee8848b11b5dbe5ffb0580
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2020-09-20 16:45:14 +0200

    formulaire

---
+++ TD1/creerVoiture.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Resultat</title>
+</head>
+<body>
+	<?php 
+		echo "<p>Immatriculation: {$_GET['immatriculation']}, Marque: {$_GET['marque']}, Couleur: {$_GET['couleur']}</p>\n";
+	?>
+</body>
+</html>
