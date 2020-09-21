fb7f34ecd09ab6950ce4122c4f77039ea306c8a5
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2020-09-21 11:22:20 +0200

    testModel.php

--- TD2/testConf.php
+++ TD2/testConf.php
@@ -6,8 +6,15 @@
 </head>
 <body>
     <?php
-        require_once "Model.php";
-        echo "Connexion réussie !" ;
+        // On inclut les fichiers de classe PHP avec require_once
+        // pour éviter qu'ils soient inclus plusieurs fois
+        require_once 'Conf.php';
+
+        // On affiche le login de la base de donnees
+        echo Conf::getLogin();
+        echo Conf::getHostname();
+        echo Conf::getDatabase();
+        echo Conf::getPassword();
     ?>
 </body>
 </html>
