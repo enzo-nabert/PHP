0de67cbb7418930161eb394d69c3afdf7154abc0
enzo-nabert <enzo.nabert@etu.umontpellier.fr>
2020-09-21 11:16:35 +0200

    exercice 4 pdo

--- TD2/testConf.php
+++ TD2/testConf.php
@@ -6,15 +6,8 @@
 </head>
 <body>
     <?php
-        // On inclut les fichiers de classe PHP avec require_once
-        // pour éviter qu'ils soient inclus plusieurs fois
-        require_once 'Conf.php';
-
-        // On affiche le login de la base de donnees
-        echo Conf::getLogin();
-        echo Conf::getHostname();
-        echo Conf::getDatabase();
-        echo Conf::getPassword();
+        require_once "Model.php";
+        echo "Connexion réussie !" ;
     ?>
 </body>
 </html>
